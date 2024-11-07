ec95eb74bed9818a8bf8aae71bd717aa440baea9
Eslam.khder <eslamkhder11@gmail.com>
2024-11-07 18:24:30 +0200

    lec10

---
+++ lec11/spring-demo-web/src/main/java/com/spring/config/RestExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.spring.config;
+
+import com.spring.dto.ExceptionResponse;
+import com.spring.service.impl.BundleTranslatorService;
+import jakarta.transaction.SystemException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(SystemException.class)
+        public ResponseEntity<ExceptionResponse> handelException(SystemException exception) {
+            return ResponseEntity.ok(new ExceptionResponse(HttpStatus.BAD_REQUEST, BundleTranslatorService.getMessageInEnglishAndArabic(exception.getMessage())));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ExceptionResponse> handelException(RuntimeException exception) {
+        return ResponseEntity.ok(new ExceptionResponse(HttpStatus.BAD_REQUEST,  BundleTranslatorService.getMessageInEnglishAndArabic(exception.getMessage())));
+    }
+}
