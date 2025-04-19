aff943654c3ad730b154d947e09a468fd7533bae
Eslam.khder <eslamkhder11@gmail.com>
2025-04-19 21:38:29 +0200

    security

--- lec7/spring-boot-903/src/main/java/com/spring/boot/SpringBoot903Application.java
+++ lec7/spring-boot-903/src/main/java/com/spring/boot/SpringBoot903Application.java
@@ -1,6 +1,7 @@
 package com.spring.boot;
 
 import com.spring.boot.dto.MathNumber;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
@@ -9,35 +10,7 @@ import java.util.List;
 public class SpringBoot903Application {
 
 	public static void main(String[] args) {
-
+		SpringApplication.run(SpringBoot903Application.class, args);
 	}
 
-	public static <T extends Number> Double sum(T x, T y){
-		return x.doubleValue() + y.doubleValue();
-	}
-
-
-	public static <T> void display(T value){
-		System.out.println(value);
-	}
-
-	/*
-	public static int sum(int x, int y){
-		return x + y;
-	}
-
-
-	public static float sum(float x, float y){
-		return x + y;
-	}
-
-
-	public static double sum(double x, double y){
-		return x + y;
-	}
-
-
-	public static Long sum(Long x, Long y){
-		return x + y;
-	}*/
 }
---
+++ lec7/spring-boot-903/src/main/java/com/spring/boot/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.spring.boot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        return userDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/account/**").hasAnyRole("ADMIN");
+                //.requestMatchers(HttpMethod.GET, "/account/search/phone").hasAnyRole("ADMIN", "USER");
+
+
+        http.httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+    // level only user
+    // in memory
+    // database
+    /*@Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        // {noop}ahmed123
+        // {bcrypt}$2a$12$9fgj5ZjK1DjrNRApCmkYMu7ydLuTyPrMiDPTFhd3K0xNvWovvHsfC
+        UserDetails userDetails1 = User.withUsername("ahmed").password("{noop}ahmed123").roles("USER", "ADMIN").build();
+        UserDetails userDetails2 = User.withUsername("eslam").password("{noop}eslam123").roles("USER").build();
+        UserDetails userDetails3 = User.withUsername("mona").password("{noop}mona123").roles("MANGER").build();
+
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2, userDetails3);
+    }*/
+}
