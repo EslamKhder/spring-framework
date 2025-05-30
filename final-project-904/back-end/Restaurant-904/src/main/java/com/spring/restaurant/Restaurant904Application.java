d4271fff3547c3724716f6608c72e164ae9d623e
Eslam.khder <eslamkhder11@gmail.com>
2025-05-30 17:10:37 +0300

    init

---
+++ final-project-904/back-end/Restaurant-904/src/main/java/com/spring/restaurant/Restaurant904Application.java
@@ -0,0 +1,13 @@
+package com.spring.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restaurant904Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Restaurant904Application.class, args);
+    }
+
+}
