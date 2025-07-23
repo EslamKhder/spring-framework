2022e252acd4d0236dadb60d78eb93cbb3d7ad55
Eslam.khder <eslamkhder11@gmail.com>
2025-07-23 16:21:57 +0300

    tasks

--- lec7/spring-boot-905/spring-boot-905/src/main/java/com/eraasoft/spring/controller/vm/ExceptionResponse.java
+++ lec7/spring-boot-905/spring-boot-905/src/main/java/com/eraasoft/spring/controller/vm/ExceptionResponse.java
@@ -5,22 +5,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ExceptionResponse {
     private String message;
-
-    public ExceptionResponse(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
--- lec7/spring-boot-905/spring-boot-905/src/main/java/com/eraasoft/spring/model/EraaSoftSchool.java
+++ lec7/spring-boot-905/spring-boot-905/src/main/java/com/eraasoft/spring/model/EraaSoftSchool.java
@@ -7,10 +7,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-//@Getter
-//@Setter
-//@NoArgsConstructor // default
-//@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor // default
+@AllArgsConstructor
 public class EraaSoftSchool {
 
     @Id
@@ -24,35 +24,4 @@ public class EraaSoftSchool {
 
     private Integer age;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
